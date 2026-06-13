9faa7465689eb631fec3510bef1ae0db69088410
Fernanda <mariafrontend.info@gmail.com>
2026-06-13 16:21:37 -0300

    fix: use SVG flags from flagcdn.com for crisp rendering at any size

--- src/components/ui/Flag.jsx
+++ src/components/ui/Flag.jsx
@@ -2,7 +2,7 @@ export function Flag({ code, className = 'h-5 w-auto' }) {
   if (!code) return null
   return (
     <img
-      src={`https://flagcdn.com/w40/${code}.png`}
+      src={`https://flagcdn.com/${code}.svg`}
       alt=""
       className={className}
     />
